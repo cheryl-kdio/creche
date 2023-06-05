33115bef3468804b5107488845a82d8cfcb9bdf3
cherry-san <123456789+cheryl.s.kouadio@gmail.com>
2023-06-05 15:28:31 +0200

    Depot initial

---
+++ index.js
@@ -0,0 +1,7 @@
+function classToggle() {
+  const navLinks = document.querySelectorAll('.Box2');
+
+  navLinks.forEach(navLink => navLink.classList.toggle('navbar__toggleshow'));
+}
+
+document.querySelector('.navbar__link-toggle').addEventListener('click', classToggle);
