fbd3d2efab3bec58b96fe29eab5ff3be8e28f227
LilKotei <90710665+LilKotei@users.noreply.github.com>
2023-06-11 15:42:34 +0200

    Bug Info fixed

--- mechanics.js
+++ mechanics.js
@@ -5,3 +5,63 @@ function classToggle() {
 }
 
 document.querySelector('.navbar__link-toggle').addEventListener('click', classToggle);
+
+
+let ImageID = 0;
+
+function fadeIn(element) {
+  element.style.opacity = 0;
+  let opacity = 0;
+
+  let fadeInterval = setInterval(function() {
+    opacity += 0.05;
+    element.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(fadeInterval);
+    }
+  }, 50);
+}
+
+function fadeOut(element) {
+  let opacity = 1;
+
+  let fadeInterval = setInterval(function() {
+    opacity -= 0.05;
+    element.style.opacity = opacity;
+
+    if (opacity <= 0) {
+      clearInterval(fadeInterval);
+    }
+  }, 50);
+}
+
+function changeImage() {
+  ImageID++;
+  if (ImageID > 5) {
+    ImageID = 0;
+  }
+  let carouselImage = document.querySelector('.CarrouselImage');
+
+  fadeOut(carouselImage);
+
+  setTimeout(function() {
+    if (ImageID == 0) {
+      carouselImage.src = "Image/Loc1.jpg";
+    } else if (ImageID == 1) {
+      carouselImage.src = "Image/Loc2.jpg";
+    } else if (ImageID == 2) {
+      carouselImage.src = "Image/Loc3.jpg";
+    } else if (ImageID == 3) {
+      carouselImage.src = "Image/Loc4.jpg";
+    } else if (ImageID == 4) {
+      carouselImage.src = "Image/Loc5.jpg";
+    }
+
+    fadeIn(carouselImage);
+  }, 500);
+}
+
+
+
+window.setInterval(changeImage, 3000);
