6b166c8df94691388f9bdf6ff37e3d3b595a1a6c
LilKotei <90710665+LilKotei@users.noreply.github.com>
2023-06-11 14:56:42 +0200

    Navbar fixed

--- index.js
+++
@@ -1,7 +0,0 @@
-function classToggle() {
-  const navLinks = document.querySelectorAll('.Box2');
-
-  navLinks.forEach(navLink => navLink.classList.toggle('navbar__toggleshow'));
-}
-
-document.querySelector('.navbar__link-toggle').addEventListener('click', classToggle);
